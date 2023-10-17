a42661abf09c3450864bc984ad69eecbdae6eecc
Ruben Santana <rubensantana@outlook.com>
2023-10-17 15:51:00 -0400

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export { 
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+    
+}
